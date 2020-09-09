effcb21b371714bcf4768093dc0fb23655d147cd
c2yp71cd3v <josephferuglio@gmail.com>
2020-09-09 14:55:49 -0700

    Updated the main with some comments

--- encoder.cpp
+++ encoder.cpp
@@ -3,9 +3,26 @@
 //
 
 #include "encoder.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string sortingAlgo = argv[1];
+
+    if(sortingAlgo.compare("insertion") == 0){
+        cout << "insertion" << endl;
+        // call the insertion sort algorithm with the redirected input file
+    } else if(sortingAlgo.compare("quick") == 0){
+        cout << "quicksort" << endl;
+        // call the quicksort algorithm with the redirected input file
+    } else {
+        cout << "Incorrect sorting algorithm input, please use 'insertion' or 'quick'" << endl;
+    }
+
+
 }
