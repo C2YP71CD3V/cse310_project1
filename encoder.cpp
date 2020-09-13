44b30facf15c72212caa31383e206dc427e2ab0f
c2yp71cd3v <josephferuglio@gmail.com>
2020-09-13 15:13:42 -0700

    changed a lot, reworked 2d array to 1d array, changed from char arrays to string arrays, completed insertion sort

--- encoder.cpp
+++ encoder.cpp
@@ -14,33 +14,73 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
     string sortingAlgo = argv[1];
-    string input, totalStr;
+    string input;
 
 
     if(sortingAlgo == "insertion"){
 
         int strIndex = 0;
         while(getline(cin, input)){
-            char nullCh = '\0';
             int size = input.length();
-            char strArray2D[size][size];
-            char *(strArray)[size];
-            for(int row = 0; row < size; row++){
-                strIndex = row;
-                for(int column = 0; column < size; column++){
-                    if(strIndex == size){
-                        strIndex = 0;
+            string strArray1D[size];
+            string* strPtrArray = strArray1D;
+            //string originalStr = "";
+
+            // setting up the 1d array
+            int inputIndex = 0;
+            for(int i = 0; i < size; i++){
+                inputIndex = i;
+                for(int j = 0; j < size; j++){
+                    if(inputIndex == size){
+                        inputIndex = 0;
                     }
-                    strArray2D[row][column] = input[strIndex];
-                    strIndex++;
+                    strArray1D[i] += input[inputIndex];
+                    inputIndex++;
                 }
-                strArray2D[row][size] = nullCh;
-                for(int index = 0; index < size; index++){
-                    strArray[index] = strArray2D[index];
-                    //cout << "index:" << index << " " << strArray[index] << endl;
-                }
-                //cout << "index:" << row << " " << strArray[row] << endl;
             }
+
+            // setting up the pointer array
+            for(int j = 0; j < size;j++){
+                strPtrArray[j] = strArray1D[j];
+            }
+
+            string originalStr = strPtrArray[0];
+
+            //insertion sort on pointer array
+            for(int index = 1; index < size; index++){
+                for(int m = index -1; m >= 0; m--){
+                    if(strPtrArray[m].compare(strPtrArray[m + 1]) > 0) {
+                        string temp = strPtrArray[m + 1];
+                        strPtrArray[m + 1] = strPtrArray[m];
+                        strPtrArray[m] = temp;
+                    }
+                }
+            }
+
+            for(int k = 0; k < size; k++){
+                cout << "index:" << k << " " << strPtrArray[k] << endl;
+            }
+
+            // insertion sort
+
+           /* while( n < size - 1){
+                for(int i = n; i > 0; i--){
+
+                    for(int j = i -1; j >= 0; j--){
+
+                        if(strArray[j] > strArray[i]){
+                            char* temp = strArray[j];
+                            strArray[j] = strArray[i];
+                            strArray[i] = temp;
+                        } else {
+                            i = 0;
+                            break;
+                        }
+                    }
+                }
+                n++;
+            } */
+
 
         }
 
